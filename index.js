53c3af60a8ef8845eabb2eb8825466ba7794989a
jameenraj16 <53219182+jameenraj16@users.noreply.github.com>
2024-03-01 21:17:30 +0530

    commit

--- index.js
+++ index.js
@@ -5,18 +5,16 @@ import dotenv from "dotenv"
 import productRouter from './routes/productRoutes.js'
 import userRouter from './routes/userRoutes.js'
 import path from "path"
-import fileUpload from 'express-fileupload'
+import multer from 'multer'
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
-app.use(fileUpload())
 dotenv.config()
 
 const port = 5000
 const password = process.env.MONGODB_PASSWORD || "YFBEFaktbSAk2ixX"
-
 
 
 /* MongoDB connection */
@@ -34,38 +32,7 @@ app.get('/', (req, res) => {
 })
 app.use("/product", productRouter)
 app.use("/user", userRouter)
-
-/* File Upload */
-
-app.post('/upload', (req, res) => {
-    let product;
-    let uploadPath;
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).json({
-            success: 0,
-            message: 'No files were uploaded.'
-        });
-    }
-    const uploadDir = './upload/images';
-    product = req.files.product;
-    uploadPath = path.join(process.cwd(), uploadDir, product.name);
-    product.mv(uploadPath, function (err) {
-        if (err) {
-            return res.status(500).json({
-                success: 0,
-                message: 'Error uploading file.'
-            });
-        }
-        res.json({
-            success: 1,
-            message: 'File uploaded successfully',
-            image_URL: uploadPath // Assuming uploadPath contains the URL of the uploaded image
-        });
-    });
-});
-
-
-/* const storage = multer.diskStorage({
+const storage = multer.diskStorage({
     destination: './upload/images',
     filename: (req, file, cb) => {
         return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
@@ -85,4 +52,4 @@ app.post('/upload', upload.single('product'), (req, res) => {
     } catch (error) {
         console.log(error);
     }
-}) */
+})
